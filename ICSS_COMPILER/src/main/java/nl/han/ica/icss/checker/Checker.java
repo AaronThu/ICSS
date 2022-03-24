af4e1a35261814323c076682692bec01112c46d0
Aaron Thunnissen <aaronthunnissen@live.nl>
2022-03-24 22:29:50 +0100

    Update Checker.java
    - Added declaration checker (working)
    - Added expressiontype checker (working)
    - Added operation checker (not working)

--- ICSS_COMPILER/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ ICSS_COMPILER/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,6 +3,9 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -14,45 +17,88 @@ public class Checker {
     public void check(AST ast) {
         variableTypes = new HANLinkedList<>();
 //        checkNodes(ast.root);
-        checkEntireStyleSheet(ast.root);
+        checkerEntireStyleSheet(ast.root);
     }
 
-    private void checkEntireStyleSheet(ASTNode astNode){
+    private void checkerEntireStyleSheet(ASTNode astNode){
         for (ASTNode childNode : astNode.getChildren()){
 
             if(childNode instanceof VariableAssignment){
-                checkVariableAssignment(childNode);
+                checkerVariableAssignment(childNode);
             }
             if(childNode instanceof Stylerule){
-                checkStyleRule(childNode);
+                checkerStyleRule(childNode);
             }
         }
     }
 
-    private void checkStyleRule(ASTNode astNode) {
+    private void checkerStyleRule(ASTNode astNode) {
         for(ASTNode childNode : astNode.getChildren()){
-            System.out.println(childNode.getNodeLabel());
-        }
-    }
-
-    private void checkVariableAssignment(ASTNode childNode) {
-    }
-
-
-    private void checkNodes(ASTNode astNode) {
-        for(ASTNode childNode : astNode.getChildren()){
-            System.out.println(childNode.getNodeLabel());
-            if(childNode.getChildren() != null) {
-                checkNodes(childNode);
-            } else{
-                checkNode(childNode);
+            if(childNode instanceof Declaration){
+                checkerDeclaration(childNode);
             }
         }
     }
 
-    private void checkNode(ASTNode astNode){
-        Declaration d = (Declaration) astNode;
-        System.out.println(d.expression.toString());
+    private void checkerDeclaration(ASTNode astNode) {
+        Declaration declaration = (Declaration) astNode;
+        String propertyName = declaration.property.name;
+        ExpressionType expression = checkExpressionType(declaration);
+
+        switch(propertyName){
+            case("width"):
+                if(expression != ExpressionType.PERCENTAGE || expression != ExpressionType.PIXEL){
+                    astNode.setError("Width can only be in percentage or pixel literals" + astNode.toString());
+                }
+                break;
+            case("height"):
+                if(expression != ExpressionType.PERCENTAGE || expression != ExpressionType.PIXEL){
+                    astNode.setError("Height can only be in percentage or pixel literals" + astNode.toString());
+                }
+                break;
+            case("color"):
+                if(expression != ExpressionType.COLOR){
+                    astNode.setError("Color can only be in percentage or pixel literals" + astNode.toString());
+                }
+                break;
+            case("background-color"):
+                if(expression != ExpressionType.COLOR){
+                    astNode.setError("Background-color can only be in percentage or pixel literals" + astNode.toString());
+                }
+                break;
+        }
+    }
+
+
+    private ExpressionType checkExpressionType(Declaration declaration){
+        Expression expression = declaration.expression;
+
+        if(expression instanceof ColorLiteral){
+            return ExpressionType.COLOR;
+        }
+        if(expression instanceof PercentageLiteral){
+            return ExpressionType.PERCENTAGE;
+        }
+        if(expression instanceof PixelLiteral){
+            return ExpressionType.PIXEL;
+        }
+        if(expression instanceof Operation){
+            return checkOperation(expression);
+        }
+
+        return ExpressionType.UNDEFINED;
+    }
+
+    private ExpressionType checkOperation(Expression expression) {
+        Operation operation = (Operation) expression;
+
+        System.out.println(operation.lhs.toString());
+        System.out.println(operation.rhs.toString());
+return null;
+    }
+
+
+    private void checkerVariableAssignment(ASTNode childNode) {
     }
 
 }
