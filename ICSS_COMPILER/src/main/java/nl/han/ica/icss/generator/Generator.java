dd18dc58d3c58af0912cdf3d6c649590a51d20d7
Aaron Thunnissen <aaronthunnissen@live.nl>
2022-03-25 14:42:53 +0100

    Added Basic Generator & Transformator

--- ICSS_COMPILER/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ ICSS_COMPILER/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,70 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Generator {
 
-	public String generate(AST ast) {
-        return "";
+	private StringBuilder sb;
 
-
+	public Generator(){
+		this.sb = new StringBuilder();
 	}
 
-	
+	public String generate(AST ast) {
+        generateNode(ast.root);
+		return sb.toString();
+	}
+
+	private void generateNode(ASTNode rootNode) {
+		for(ASTNode childNode : rootNode.getChildren()){
+			if(childNode instanceof Stylerule){
+				generateSelector(childNode);
+				generateDeclaration(childNode);
+			}
+		}
+	}
+
+	private void generateDeclaration(ASTNode rootNode) {
+		for(ASTNode childNode : rootNode.getChildren()){
+			if(childNode instanceof Declaration){
+				Declaration declaration = (Declaration) childNode;
+				sb.append(" ")
+						.append(declaration.property.name)
+						.append(": ")
+						.append(generateStringFromExpression(declaration.expression))
+						.append(";\n");
+
+			}
+		}
+	}
+
+	private String generateStringFromExpression(Expression expression) {
+		if(expression instanceof ColorLiteral){
+			return ((ColorLiteral) expression).value +"";
+		}
+
+		if(expression instanceof PixelLiteral){
+			return ((PixelLiteral) expression).value +"";
+		}
+
+		return "";
+	}
+
+	private void generateSelector(ASTNode rootNode) {
+		Stylerule stylerule = (Stylerule) rootNode;
+
+		List<String> allSelectors = stylerule.selectors.stream().map(ASTNode::toString).collect(Collectors.toList());
+
+		String selectorString = String.join("",allSelectors);
+		sb.append(selectorString);
+		sb.append(" {\n");
+	}
+
+
 }
--- ICSS_COMPILER/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ ICSS_COMPILER/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -2,6 +2,7 @@ package nl.han.ica.icss.transforms;
 
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
+import nl.han.ica.datastructures.SymbolTable;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.PercentageLiteral;
 import nl.han.ica.icss.ast.literals.PixelLiteral;
@@ -10,22 +11,61 @@ import nl.han.ica.icss.ast.operations.AddOperation;
 import nl.han.ica.icss.ast.operations.MultiplyOperation;
 import nl.han.ica.icss.ast.operations.SubtractOperation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Evaluator implements Transform {
 
-    private IHANLinkedList<HashMap<String, Literal>> variableValues;
+    private SymbolTable<String, Literal> variableValues;
 
     public Evaluator() {
-        variableValues = new HANLinkedList<>();
+        variableValues = new SymbolTable<>();
     }
 
     @Override
     public void apply(AST ast) {
-        variableValues = new HANLinkedList<>();
-
+        transformStyleSheet(ast.root);
     }
 
-    
+    private void transformStyleSheet(Stylesheet stylesheet){
+
+        variableValues.push();
+
+        for(ASTNode childNode : stylesheet.getChildren()){
+            if(childNode instanceof Stylerule){
+                transformStyleRule((Stylerule) childNode);
+            }
+        }
+    }
+
+    private void transformStyleRule(Stylerule stylerule){
+        ArrayList<ASTNode> astNodesToAdd = new ArrayList<>();
+        variableValues.push();
+
+        for(ASTNode childNode: stylerule.body){
+            transformStyleBody(childNode, astNodesToAdd);
+        }
+    }
+
+    private void transformStyleBody(ASTNode astNode, ArrayList<ASTNode> parent) {
+        if(astNode instanceof Declaration){
+            transformDeclaration((Declaration) astNode);
+            parent.add(astNode);
+        }
+    }
+
+    private void transformDeclaration(Declaration declaration){
+        declaration.expression = transformExpression(declaration.expression);
+    }
+
+    private Literal transformExpression(Expression expression) {
+        if(expression instanceof VariableReference){
+            return variableValues.getVariableByKey((((VariableReference) expression).name));
+        }
+        System.out.println(expression);
+        return (Literal) expression;
+    }
+
+
 }
